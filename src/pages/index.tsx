8ddc7a89ee93b7e916770c73cff2c49ec0da84cd
David Blankenship <david.blankenship@yale.edu>
2023-07-15 12:01:42 -0400

    fix(prisma): Add event type to page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 import { api } from "~/utils/api";
+import { Event } from "@prisma/client";
 
 export default function Home() {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
